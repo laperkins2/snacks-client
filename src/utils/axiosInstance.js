77f63a4a2a1ccd3b0fed339c52e6ca25e623334b
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-09-16 20:37:22 -0500

    installed axios

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 1000,
+  headers: {
+    'api-key': 'process.env.NEXT_PUBLIC_API_KEY',
+  },
+});
+
+export default instance;
