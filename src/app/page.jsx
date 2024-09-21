82830eff924acd8441b3a2f7a3179a3582acfeb6
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-09-21 17:04:13 -0500

    change home page file name to jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,23 @@
+'use client';
+import Image from 'next/image';
+import snacksAPI from '@/utils/axiosInstance';
+import { useEffect, useState } from 'react';
+import SnackCard from '@/components/SnackCard';
+import { useSnacks } from '@/contexts/snackContext';
+import { Suspense } from 'react';
+
+export default function Home() {
+  const { snacks } = useSnacks();
+  return (
+    <Suspense fallback={Loading}>
+      <div>
+        {snacks &&
+          snacks.map((data, index) => {
+            return <SnackCard key={index} snack={data} />;
+          })}
+      </div>
+    </Suspense>
+  );
+}
+
+export const Loading = <h2>Loading... </h2>;
