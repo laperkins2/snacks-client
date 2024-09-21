9ca347bae2108af65aa9b8981793f95ddd45f455
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-09-21 17:00:44 -0500

    updated instance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 1000,
+  baseURL: process.env.NEXT_PUBLIC_SNACKS_API_URL,
+  timeout: 500000,
   headers: {
-    'api-key': 'process.env.NEXT_PUBLIC_API_KEY',
+    'api-key': 'process.env.NEXT_PUBLIC_SNACKS_API_KEY',
   },
 });
 
