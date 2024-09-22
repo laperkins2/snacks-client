7b70ab688fc8e5c11e718f589c434ac5d32b16c3
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-09-21 19:14:03 -0500

    removed quotation marks in instance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SNACKS_API_URL,
   timeout: 500000,
   headers: {
-    'api-key': 'process.env.NEXT_PUBLIC_SNACKS_API_KEY',
+    'api-key': process.env.NEXT_PUBLIC_SNACKS_API_KEY,
   },
 });
 
